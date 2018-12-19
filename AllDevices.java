b1511a532e02ca8f61ccf3cb1aeb11bc25bc7822
ElenaGrischenko <45994284+ElenaGrischenko@users.noreply.github.com>
2018-12-19 12:43:52 +0200

    Add files via upload

---
+++ AllDevices.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class AllDevices {
+    public double time;
+    public boolean ON;
+    public double I;
+
+    public AllDevices(double time, boolean ON, double I){
+        this.time= 0;
+        this.ON=ON;
+        this.I=0;
+    }
+
+}
---
+++ Generator.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Generator extends AllDevices{
+    public int Users;
+    public int AddUsers;//
+    public int AddUsers;//
+
+}
